2a8a09dbc310b550dce53170a33c58a915773a80
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2017-09-01 00:25:36 +0100

    Added functions and classes files to the windows client project with some pre-writen code

---
+++ Shallow Blue Client (Windows)/Classes.vb
@@ -0,0 +1,44 @@
+﻿Module Classes
+
+    Class JSON_Class_Extracter
+
+        Private C_classType As String
+
+        Public Property classType() As String
+            Get
+                Return C_classType
+            End Get
+            Set(ByVal value As String)
+                C_classType = value
+            End Set
+        End Property
+
+        Private C_JSON_Text As String
+
+        Public Property JSON_Text() As String
+            Get
+                Return C_JSON_Text
+            End Get
+            Set(ByVal value As String)
+                C_JSON_Text = value
+            End Set
+        End Property
+
+    End Class
+
+    Class Temp
+
+        Private C_tempVar As String
+
+        Public Property tempVar() As String
+            Get
+                Return C_tempVar
+            End Get
+            Set(ByVal value As String)
+                C_tempVar = value
+            End Set
+        End Property
+
+    End Class
+
+End Module
---
+++ Shallow Blue Client (Windows)/Functions.vb
@@ -0,0 +1,70 @@
+﻿Imports System.Net
+Imports System.IO
+Imports System.Text
+Imports Newtonsoft.Json
+
+Module Functions
+
+    Public Function serverRequest(sendData)
+
+        Dim request As HttpWebRequest = DirectCast(WebRequest.Create("http://localhost:5555/test"), HttpWebRequest)
+
+        request.Method = "POST"
+
+        Dim dataStream As New StreamWriter(request.GetRequestStream())
+
+        dataStream.Write(sendData)
+
+        dataStream.Close()
+
+        Dim responce As HttpWebResponse = DirectCast(request.GetResponse(), HttpWebResponse)
+
+        Dim reader As StreamReader = New StreamReader(responce.GetResponseStream())
+
+        Dim stringData As String = reader.ReadToEnd()
+
+        reader.Close()
+
+        Dim data = JSONConverter(stringData)
+
+        Return data
+
+    End Function
+
+    Public Function JSONConverter(jsonData As String, Optional classType As String = vbNullString)
+
+        'Example json data as a string [{"classType":"Temp","JSON_Text":"{'tempVar':'Hello'}"},{"classType":"Temp","JSON_Text":"{'tempVar':'World'}"}]
+
+        Dim firstDeserialisation
+
+        If classType = vbNullString Or classType = "JSON_Class_Extracter" Then
+
+            firstDeserialisation = JsonConvert.DeserializeObject(Of JSON_Class_Extracter())(jsonData)
+
+        ElseIf classType = "Temp" Then
+
+            Return JsonConvert.DeserializeObject(Of Temp)(jsonData)
+
+        Else
+
+            firstDeserialisation = {}
+
+            'error Class not recognised
+
+            'Return New ClassNotRecognised()
+
+        End If
+
+        Dim deSerialisedData As New ArrayList
+
+        For Each JSON_Object As JSON_Class_Extracter In firstDeserialisation
+
+            deSerialisedData.Add(JSONConverter(JSON_Object.JSON_Text, JSON_Object.classType))
+
+        Next
+
+        Return deSerialisedData
+
+    End Function
+
+End Module
