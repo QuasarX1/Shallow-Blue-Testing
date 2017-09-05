cb5e30b671866093f6ab7017bc0d01f1e22b2e7c
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2017-09-05 17:55:00 +0100

    Added a test nav bar with a style sheet to the website

--- Shallow Blue Website/app.py
+++ Shallow Blue Website/app.py
@@ -19,7 +19,7 @@ def hello():
 @app.route('/test')
 def test():
     """Renders a test page."""
-    return  render_template("MainPage.html")
+    return  render_template("MainPage.html", pageTitle = "test page", url = "/test")
 
 if __name__ == '__main__':
     import os
@@ -28,4 +28,4 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT)
+    app.run(HOST, PORT, threaded = True, debug = True)
