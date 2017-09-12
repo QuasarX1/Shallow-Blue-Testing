d54f6bb4d1e80a372b8a813ab8aed6d92a13700f
QuasarX1 - BuildTools <chris.rowe19@outlook.com>
2017-09-13 00:49:45 +0100

    Added a new test virtual environment along with some SQLAlchemy code to the website project

--- Shallow Blue Website/app.py
+++ Shallow Blue Website/app.py
@@ -3,10 +3,27 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
+import os
 from Classes import *
 import datetime
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+baseDir = os.path.abspath(os.path.dirname(__file__))
+
+dbFileName = input("Database file name:\n")
+
+print("sqlite:///" + os.path.join(baseDir, os.path.join("databases", dbFileName)))
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(baseDir, os.path.join("databases", dbFileName))
+
+app.config["SQLALCHMEY_COMMIT_ON_TEARDOWN"] = True
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
@@ -19,7 +36,7 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def Main_Page():
-    """Renders a test page."""
+    """Renders the main page."""
     return  render_template("MainPage.html", pageTitle = "Main Page", url = "/")
 
 @app.route("/data/news-bar")
@@ -29,10 +46,9 @@ def newsBar():
     return str(time.hour) + ":" + str(time.minute) #Return query to db as to lates news
 
 if __name__ == '__main__':
-    import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, threaded = True, debug = True)
+    app.run(HOST, PORT, threaded = True, debug = False)
